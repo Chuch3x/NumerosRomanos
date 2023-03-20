6885720e26616a3d3e405afead55f040dd0e477c
Cesar <c.augusto_mendez@hotmail.com>
2023-03-20 18:37:29 -0400

    Ciclo 1: para el numero 1 muestra I

---
+++ src/romanos.test.js
@@ -0,0 +1,12 @@
+function convertir_a_romano(num){
+    return "I";
+}
+
+
+describe("FizzBuzz", () => {
+    it("deberia mostrar el numero I para el numero 1", () => {
+      expect(convertir_a_romano(1)).toEqual("I");
+    });
+    
+  });
+  
