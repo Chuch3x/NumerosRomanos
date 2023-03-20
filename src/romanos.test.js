5ee5be0005d89d21c093464df7fcfcf7f41f6858
Cesar <c.augusto_mendez@hotmail.com>
2023-03-20 18:53:24 -0400

    Ciclo 2: para el numero 2 muestra II

--- src/romanos.test.js
+++ src/romanos.test.js
@@ -1,5 +1,11 @@
 function convertir_a_romano(num){
-    return "I";
+    if(num == 1){
+        return "I";
+    }
+    else{
+        return "II";
+    }
+    
 }
 
 
@@ -7,6 +13,9 @@ describe("FizzBuzz", () => {
     it("deberia mostrar el numero I para el numero 1", () => {
       expect(convertir_a_romano(1)).toEqual("I");
     });
+    it("deberia mostrar el numero II para el numero 2", () => {
+        expect(convertir_a_romano(2)).toEqual("II");
+      });
     
   });
   
