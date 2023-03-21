58187fabead20eefdb149bd0aaae7f3304600bb1
Cesar <c.augusto_mendez@hotmail.com>
2023-03-20 21:30:34 -0400

    Para el numero 5 muestra V

--- src/romanos.test.js
+++ src/romanos.test.js
@@ -5,8 +5,11 @@ function convertir_a_romano(num){
     else if(num == 2){
         return "II";
     }
-    else{
+    else if(num == 3){
         return "III";
+    }
+    else{
+        return "V";
     }
     
 }
@@ -19,8 +22,11 @@ describe("FizzBuzz", () => {
     it("deberia mostrar el numero II para el numero 2", () => {
         expect(convertir_a_romano(2)).toEqual("II");
       });
-      it("deberia mostrar el numero III para el numero 3", () => {
+    it("deberia mostrar el numero III para el numero 3", () => {
         expect(convertir_a_romano(3)).toEqual("III");
+      });
+    it("deberia mostrar el numero V para el numero 5", () => {
+        expect(convertir_a_romano(5)).toEqual("V");
       });
   });
   
